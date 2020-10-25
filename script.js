b5a914000eeac039eace832386ec9a3bd572268c
Mateusz Choma <kontakt@coderoad.pl>
2020-10-25 12:43:57 +0100

    Waitress added

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
 const Beer = class {
-    constructor(){
+    constructor() {
         this.fill = 100
     }
 
-    drink(volume = 10){
-        if(this.fill === 0) {
+    drink(volume = 10) {
+        if (this.fill === 0) {
             console.warn('Try to drink from empty glass!')
             return
         }
         const fillBeforeDrink = this.fill
         const newFill = this.fill - volume
-        this.fill = newFill <=0 ? 0 : newFill
+        this.fill = newFill <= 0 ? 0 : newFill
         console.log(`You drank ${fillBeforeDrink - this.fill} %`)
     }
 
-    drinkWhole(){
+    drinkWhole() {
         return this.drink(100)
     }
 }
 
 const BeerStorage = class {
-    
-    constructor(startQuantity = 2){
+
+    constructor(startQuantity = 2) {
         this.beers = (new Array(startQuantity)).fill(1).map(() => new Beer())
     }
 
-    grabBeer(){
-        if(this.beers.length === 0){
+    grabBeer() {
+        if (this.beers.length === 0) {
             throw new Error('No beers in storage')
         }
         return this.beers.pop()
     }
 }
 
-const storage1 = new BeerStorage() 
+const storage1 = new BeerStorage()
+
+const Waitress = class {
+    constructor(storage) {
+        this.storage = storage
+    }
+
+    fetchBeer() {
+        return new Promise((resolve, reject) => {
+            const timeOfFetch = Math.round((Math.random() * 4)) + 1
+            console.log(`Fetching beer will take ${timeOfFetch} s`)
+
+            setTimeout(
+                () => {
+                    resolve('beer')
+                },
+                timeOfFetch * 1000
+            )
+        })
+    }
+}
+
+const waitress1 = new Waitress(storage1)
+
+const fetchBeerPromise = waitress1.fetchBeer()
+
+fetchBeerPromise.then(console.log)
+
+console.log(fetchBeerPromise)
